5657f601326b6345df1e8647acf3676b03195c43
tiffanyni47 <102348115+tiffanyni47@users.noreply.github.com>
2025-05-14 15:43:18 -0400

    Campus.js and Student.js updated
    Sequelize Campus and student, which added imageUrl to the campus
    email, imageUrl, and GPA to the student

---
+++ routes/Campus.js
@@ -0,0 +1,42 @@
+/*==================================================
+/database/models/Campus.js
+
+It defines the campus model for the database.
+==================================================*/
+const Sequelize = require('sequelize');  // Import Sequelize
+const db = require('../db');  // Import Sequelize database instance called "db"
+
+// Define the campus model
+const Campus = db.define("campus", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false，
+    validate: {
+      notEmpty: true,
+    },
+  },
+
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false，
+    validate: {
+      notEmpty: true,
+    },
+  },
+
+  description: {
+    type: Sequelize.STRING,
+  }，
+
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: true,
+    defaultValue: 'default-student.jpg',
+    validate: {
+      isUrl: true,
+    },
+  },
+});
+
+// Export the campus model
+module.exports = Campus;
---
+++ routes/Student.js
@@ -0,0 +1,52 @@
+/*==================================================
+/database/models/Student.js
+
+It defines the student model for the database.
+==================================================*/
+const Sequelize = require('sequelize');  // Import Sequelize
+const db = require('../db');  // Import Sequelize database instance called "db"
+
+const Student = db.define("student", {
+  firstname: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+
+  lastname: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+    imageUrl: {
+      type: Sequelize.STRING,
+      allowNull: true,
+      defaultValue: 'default-student.jpg',
+      validate: {
+        isUrl: true,
+      },
+    },
+
+    GPA: {
+      type: Sequelize.FLOAT,
+      allowNull: true,
+      validate: {
+        min: 0.0,
+        max: 4.0,         
+      },
+    }
+});
+
+// Export the student model
+module.exports = Student;
